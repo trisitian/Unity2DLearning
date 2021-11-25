b334454eaa5291e63085f54d469dc23e3454d70e
Tristian <tristian.labanowich@gmail.com>
2021-11-24 22:25:54 -0800

    cleanup

--- SnowBoardGame/Assets/Scripts/CrashDetector.cs
+++ SnowBoardGame/Assets/Scripts/CrashDetector.cs
@@ -8,14 +8,16 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float dealay = 1f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip effect;
+    bool hasCrashed;
 
     void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "Ground"){
+        if(other.tag == "Ground" && !hasCrashed){
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(effect);
-            Invoke("reloadScene", dealay);
+            hasCrashed = true;
         }
+        Invoke("reloadScene", dealay);
     }
     void reloadScene(){
         SceneManager.LoadScene(0);
