f4c5f4d2d6ea5ac988e8f6e302c60d1e6f9d43ce
Tristian <tristian.labanowich@gmail.com>
2021-11-24 20:58:45 -0800

    implemented boost mechanic

--- SnowBoardGame/Assets/Scripts/PlayerController.cs
+++ SnowBoardGame/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D sf2d;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        sf2d = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+
+    void RespondToBoost(){
+        if(Input.GetKey(KeyCode.UpArrow)){
+            sf2d.speed = boostSpeed;
+        }else{
+            sf2d.speed = baseSpeed;
+        }
+
+    }
+
+    void RotatePlayer(){
         if(Input.GetKey(KeyCode.LeftArrow)){
             rb2d.AddTorque(torqueAmount);
         }else if(Input.GetKey(KeyCode.RightArrow)){
